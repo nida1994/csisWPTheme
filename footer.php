871d270cbad752a16ff58407fdd5bce9d5a82e94
nida1994 <nida.b94@gmail.com>
2015-03-13 11:45:41 -0400

    Initialize tracking for project

---
+++ footer.php
@@ -0,0 +1,29 @@
+
+      </div> <!-- #wrap -->
+      <footer class ="footer">
+        
+        <nav class="site-nav">
+        
+        <hr size="30">
+        
+        <?php wp_nav_menu( $args ); ?>
+        </nav>
+      
+      </footer>
+
+    </div> <!-- /container -->
+
+        </nav>
+
+       </footer>
+      </div> <!-- /container -->
+    
+        <?php wp_footer(); ?>
+
+      </body>
+
+</div>
+
+</div>
+
+</html>
